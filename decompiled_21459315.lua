baa40bab5e04fb13ad4479d7574e098b36b85df2
Thawne <50044561+MicrosoftThawne@users.noreply.github.com>
2020-01-22 20:46:13 +0100

    Update decompiled_21459315.lua

--- decompiled_21459315.lua
+++ decompiled_21459315.lua
@@ -3,12 +3,11 @@
 local s1 = {}
 local s2 = getfenv()
 s1.s5 = "table"
-function s1.ModifyBulletValue(s3, v6)
+function s1.ModifyBulletValue(s3)
   if type(s3) ~= s1.s5 then -->> As you can see they check if the bullets are stored in a table or else it will detect you.
     return false -->> Get ready to be banned :rofl:
   end
-  s4 = { s3, *v6 } -->> [2]arg was decompiled a little weird, but it's the secondary guns ammo value.
-	s2.Bullets = s4 -->> As you can see they cache the value in 'getfenv().Bullets' as a table.
+	s2.Bullets = s3 -->> As you can see they cache the value in 'getfenv().Bullets' as a table.
   return true
 end
 
